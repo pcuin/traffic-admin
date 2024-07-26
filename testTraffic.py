9eb1e98ba85ead775b4238a3bc78d5e8c4b3936d
pcuin <cuinpablo3@gmail.com>
2024-07-26 01:22:19 -0700

    Update testTraffic.py
    name change

--- testTraffic.py
+++ testTraffic.py
@@ -5,7 +5,7 @@ class TestTraffic:
 
     def test_init(self):
 
-        d = TrafficRank(1.99, "Low", 0, 1)
+        d = Traffic(1.99, "Low", 0, 1)
         assert d.carpool == False
         assert d.risk == 1
         assert d.rkey == "Low"
@@ -14,6 +14,6 @@ class TestTraffic:
 
     def test_get_liability(self):
 
-        d = TrafficRank(1.99, "Low", 0, 1)
+        d = Traffic(1.99, "Low", 0, 1)
         assert d.get_liability() == d.risk
 
