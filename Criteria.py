d8afc850fb4c816636aa77d6b6603fd9b99ce0bc
pcuin <cuinpablo3@gmail.com>
2024-07-26 01:14:00 -0700

    Add files via upload

---
+++ Criteria.py
@@ -0,0 +1,10 @@
+# Criteria
+## will be used as an UI to direct ethics into
+## how the program will rank and lay the groundwork for this project...
+### This file is to support the infrastructure and provoke thoughts into
+### the Human-design development and ethics of coding.
+
+from traffic
+class Criteria:
+
+    
---
+++ UI.py
@@ -0,0 +1,8 @@
+# UI
+
+## This file organizes all moving parts of the menu to
+## easily separate the visual and tecnical aspects of the combined goal.
+
+class UI:
+
+    
